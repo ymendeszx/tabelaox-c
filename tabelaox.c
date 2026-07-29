3184fdb227e0afed7ea7a49124204908f5ae8604
mendes <ymendeszx@gmail.com>
2026-07-29 19:06:09 -0300

    Update tabelaox.c

--- tabelaox.c
+++ tabelaox.c
@@ -1,56 +1,57 @@
 #include <stdio.h>
-#include "function_tabela.c"
+#include "function_tabelaox.c"
 
-int main(int argc, char *argv[]) {
-    char tabela[3][3] = {{0}};
-    int usar_argumentos = 0;
+int main(int argc, char *argv[]) 
+{
+    char tabela[3][3] = {0};
+    int por_parametro = 0;
 
-    if (argc > 1) {
-        int tamanho_arg = 0;
-        while (argv[1][tamanho_arg] != '\0') {
-            tamanho_arg++;
-        }
-        if (tamanho_arg >= 9) {
-            usar_argumentos = 1;
+    if (argc > 1) 
+    {
+        int resultado = processar(argc, argv, tabela);
+        if (resultado == -1) 
+        {
+            printf("caractere invalido\n");
+            return 0;
+        } else if (resultado == 1) 
+        {
+            por_parametro = 1;
         }
     }
 
-    if (usar_argumentos == 1) {
-        int indice = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                char c = argv[1][indice];
-                if (validar_tabela_char(c) == 1) {
-                    tabela[i][j] = c;
-                }
-                indice++;
-            }
-        }
-    } else {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                char caractere;
+    if (!por_parametro) 
+    {
+        for (int i = 0; i < 3; i++) 
+        {
+            for (int j = 0; j < 3; j++) 
+            {
                 int valido = 0;
-                while (valido == 0) {
-                    printf("Digite o caractere da linha %d coluna %d:\n", i + 1, j + 1);
-                    scanf(" %c", &caractere);
-                    if (validar_tabela_char(caractere) == 1) {
-                        tabela[i][j] = caractere;
+
+                while (!valido) 
+                {
+                    printf("Digite o caractere da linha %d coluna %d: ", i + 1, j + 1);
+                    scanf(" %c", &tabela[i][j]);
+
+                    if (caractereValido(tabela[i][j])) 
+                    {
                         valido = 1;
-                    } else {
-                        printf("caractere inválido\n");
+                    } else 
+                    {
+                        printf("caractere invalido\n");
                     }
                 }
             }
         }
     }
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++) 
+        {
             printf("Linha %d Coluna %d: %c\n", i + 1, j + 1, tabela[i][j]);
         }
     }
 
-    printf("Criado por Marcelo Mendes");
+    printf("Criado por: Marcelo Mendes");
     return 0;
 }
