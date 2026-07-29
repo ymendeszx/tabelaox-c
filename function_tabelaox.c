9f6d4ac3a86e064fa542df02c67ec637d2ca0259
mendes <ymendeszx@gmail.com>
2026-07-29 19:05:11 -0300

    Refactor validation and processing functions

--- function_tabelaox.c
+++ function_tabelaox.c
@@ -1,7 +1,32 @@
-#include <stdio.h>
-
-int validar_tabela_char(char c) {
-    if (c == 'X') return 1;
-    if (c == 'O') return 1;
+int caractereValido(char c) 
+{
+    if (c == 'X' || c == 'O') 
+    {
+        return 1;
+    }
     return 0;
+}
+
+int processar(int argc, char *argv[], char tabela[3][3]) 
+{
+    if (argc < 10) 
+    {
+        return 0; 
+    }
+    
+    int k = 1;
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++) 
+        {
+            char c = argv[k][0];
+            if (!caractereValido(c)) 
+            {
+                return -1;
+            }
+            tabela[i][j] = c;
+            k++;
+        }
+    }
+    return 1;
 }
